e844065f9b3d36f7447e8135044a6726749426c1
joaovvr <jvvrego_br@hotmail.com>
2016-03-17 23:56:39 -0300

    DAOcliente e DAOfilial devidamente codificadas

--- src/banco/DAOfilial.java
+++ src/banco/DAOfilial.java
@@ -5,10 +5,95 @@
  */
 package banco;
 
+import base.Filial;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Jorge
  */
 public class DAOfilial {
+    private Conexao conn = new Conexao();
+	
+	public DAOfilial(){}
+	
+	public void adicionaFilial(Filial filial) {
+		String sql = "INSERT INTO Filial(codFilial,matricula,endereco) VALUES(?,?,?)";
+		try {
+			PreparedStatement stmt = this.conn.getCon().prepareStatement(sql);
+			stmt.setInt(1, filial.getCodFilial());
+			stmt.setString(2, filial.getMatricula());
+                        stmt.setString(3, filial.getEndereco());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+	}
+	
+    public List<Filial> recuperaFilial() {
+      String sql = "select * from Filial";
+      List<Filial> filiais = new ArrayList<>();
+ 	  try {
+            PreparedStatement stmt =  this.conn.getCon().prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                     Filial filial = new Filial();
+                     filial.setCodFilial(rs.getInt("codFilial"));
+                     filial.setMatricula(rs.getString("matricula"));
+                     filial.setEndereco(rs.getString("endereco"));
+                     filiais.add(filial);
+            }       			
+            stmt.close();
+            } catch (SQLException u) {
+       		throw new RuntimeException(u);
+            }
+ 	  return filiais;
+	}
+       
+    public void atualizaFilial(int codFilial, String enderec) {
+        String sql = "UPDATE Filial SET endereco= ? WHERE codFilial = ?";
+		try {
+			PreparedStatement stmt = this.conn.getCon().prepareStatement(sql);
+			stmt.setString(1, enderec);
+			stmt.setInt(2, codFilial);
+			stmt.execute();
+			stmt.close();	
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+    }
     
+    public void deletaFilal(int codFilial) {
+        String sql = "DELETE FROM Filial WHERE codFilial = ?";
+		try {
+			PreparedStatement stmt = this.conn.getCon().prepareStatement(sql);
+			stmt.setInt(1, codFilial);
+			stmt.execute();
+			stmt.close();	
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+    }
+    
+    public Filial buscaFilial(int codFilial) {        
+        Filial filial = new Filial();
+        String sql = "SELECT * FROM Filial WHERE codFilial = ?";
+        try {
+            PreparedStatement stmt = this.conn.getCon().prepareStatement(sql);                      
+            stmt.setInt(1, codFilial);
+            ResultSet rs = stmt.executeQuery();
+            filial.setCodFilial(rs.getInt("codFilial"));
+            filial.setMatricula(rs.getString("matricula"));
+            filial.setEndereco(rs.getString("endereco"));
+            stmt.close();	
+        } catch (SQLException u) {
+            throw new RuntimeException(u);
+        }
+        return filial;
+    }
 }
